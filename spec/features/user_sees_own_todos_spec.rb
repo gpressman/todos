23d5b3800caff5e684a4aaf3f14a43ab8b5c41e5
Gabe Pressman <pressman4747@gmail.com>
2015-11-03 22:15:10 -0500

    tests pass for the right reasons now

--- spec/features/user_sees_own_todos_spec.rb
+++ spec/features/user_sees_own_todos_spec.rb
@@ -4,8 +4,8 @@ feature "User sees own todos" do
   scenario "doesn't see others todos" do
   	Todo.create!(title: "Buy Milk", email: "someone_else@example.com")
 
-  	sign_in_as "someone@example.com"
+  	sign_in_as "sometwo@example.com"
 
-  	expect(page).not_to have_css ".todos_li", text: "Buy Milk"
+  	expect(page).not_to have_css ".todos li", text: "Buy Milk"
   end
 end
